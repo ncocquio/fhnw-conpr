4c89261966bae86ee6422dd8fb553a238296fb82
Nicola Cocquio <nicola.cocquio@students.fhnw.ch>
2018-03-11 11:59:10 +0100

    Implement fair semaphores

--- 03AS_Semaphore/src/semaphore/SemaphoreImpl.java
+++ 03AS_Semaphore/src/semaphore/SemaphoreImpl.java
@@ -1,25 +1,38 @@
 package semaphore;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public final class SemaphoreImpl implements Semaphore {
-	private int value;
+    private ConcurrentLinkedQueue<Thread> fifoQueue = new ConcurrentLinkedQueue<>();
+    private int value;
 
-	public SemaphoreImpl(int initial) {
-		if (initial < 0) throw new IllegalArgumentException();
-		value = initial;
-	}
+    public SemaphoreImpl(int initial) {
+        if (initial < 0) throw new IllegalArgumentException();
+        value = initial;
+    }
 
-	@Override
-	public int available() {
-		return value;
-	}
+    @Override
+    public int available() {
+        return value;
+    }
 
-	@Override
-	public void acquire() {
+    @Override
+    public synchronized void acquire() {
+        fifoQueue.add(Thread.currentThread());
+        while (value == 0 || !Thread.currentThread().equals(fifoQueue.peek())) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        fifoQueue.poll();
+        value--;
+        notifyAll();
+    }
 
-	}
-
-	@Override
-	public void release() {
-
-	}
+    @Override
+    public synchronized void release() {
+        value++;
+        notifyAll();
+    }
 }
